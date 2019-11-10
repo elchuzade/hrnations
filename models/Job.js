8cc6d4088813cec8afe3610185a395aaabc6cdb2
Kamran Elchuzade <elchuzade@gmail.com>
2019-11-10 13:55:10 +0300

    added job and user models

---
+++ models/Job.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const JobSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    avatar: {
+      location: {
+        type: String
+      },
+      key: {
+        type: String
+      },
+      bucket: {
+        type: String
+      },
+      originalname: {
+        type: String
+      },
+      mimetype: {
+        type: String
+      },
+      size: {
+        type: Number
+      },
+      fieldName: {
+        type: String
+      }
+    },
+    location: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String
+    },
+    companyName: {
+      type: String
+    },
+    companyDescription: {
+      type: String
+    },
+    website: {
+      type: String
+    },
+    deadline: {
+      type: Date
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = Job = mongoose.model('job', JobSchema);
