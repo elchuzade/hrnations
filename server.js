c8d07a29608c1a1fe64d5d2aafce6f560486c1a6
Kamran Elchuzade <elchuzade@gmail.com>
2019-11-10 13:51:58 +0300

    added serverjs

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
+
+const users = require('./routes/api/users');
+const jobs = require('./routes/api/jobs');
+
+const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+  // Passport middleware
+app.use(passport.initialize());
+
+// User routes
+app.use('/api/users', users);
+app.use('/api/jobs', jobs);
+
+// Serve static assets if in production (Heroku stuff)
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server started on port ${port}`);
+});
